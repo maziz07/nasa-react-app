9dcba9230f7ce5bae5f2cd4a12fb22c8f01f8866
maziz07 <mazizyr@gmail.com>
2024-06-03 13:52:38 +1000

    updated index.css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 export default function Footer(){
     return (
         <footer>
+            <div className="bgGradient">
             <div>
                 <h2>The Brutal Martian Landscape</h2>
                 <h1>APOD PROJECT</h1>
+            </div>
             </div>
             <button>
             <i class="fa-solid fa-circle-info"></i>
