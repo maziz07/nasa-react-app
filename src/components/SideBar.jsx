e58043ef24c1cdd91b534624dd0a29b59b64060f
maziz07 <mazizyr@gmail.com>
2024-06-05 07:11:52 +1000

    updated

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,6 +1,7 @@
 export default function SideBar(){
     return(
-        <div>
+        <div className="sidebar">
+            <div className="bgOverlay"></div>
             <h2>The Brutal Martian Landscape</h2>
             <div>
                 <p>Description</p>
